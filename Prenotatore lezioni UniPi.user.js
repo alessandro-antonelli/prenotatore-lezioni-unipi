13132180a0b152cc6f0c159977afab495f2c115e
alessandro-antonelli <alessandroantonelli2@gmail.com>
2021-09-07 23:01:49 +0200

    base della UI

--- Prenotatore lezioni UniPi.user.js
+++ Prenotatore lezioni UniPi.user.js
@@ -5,13 +5,60 @@
 // @description  Prenota le lezioni in presenza dell'Università di Pisa
 // @author       Alessandro Antonelli
 // @match        https://agendadidattica.unipi.it/*
-// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @icon         https://www.google.com/s2/favicons?domain=unipi.it
 // @grant        none
 // ==/UserScript==
 // Source code repository: https://github.com/alessandro-antonelli/prenotatore-lezioni-unipi
 
-(function() {
-    'use strict';
+var OraAvvio;
 
-    // Your code here...
+(function()
+{
+	'use strict';
+
+	OraAvvio = new Date();
+
+	if(VerificaCaricamentoPagina() == -1) return;
+	//InstallaNeutralizzatoreAlert();
+	CaricaUI();
+	//CaricaImpostazioni();
+	//await VerificaCaricamentoAgenda();
+
+	//await ElencaSlot();
+	//CalcolaProssimoRiavvio();
+	//await EseguiPrenotazioni();
 })();
+
+function VerificaCaricamentoPagina()
+{
+	const UrlAttuale = window.location.href;
+	const UrlPrenotazioni = 'https://agendadidattica.unipi.it/Prod'
+
+	if(UrlAttuale != UrlPrenotazioni) return -1;
+	else return 0;
+}
+
+function CaricaUI()
+{
+	// Inietto CSS
+	const CSS =
+	`<style>
+		
+	</style>`;
+	document.querySelector('head').innerHTML += CSS;
+
+	// Inserisco casella dell'app
+	const ContenutoRiquadroApp =
+		`<h2>🤖Prenotatore lezioni UniPi</h2>
+		Bla bla bla`;
+	
+		var RiquadroApp = document.createElement('div');
+		RiquadroApp.id = 'RiquadroApp';
+		RiquadroApp.style.padding = '8px';
+		RiquadroApp.style.borderRadius = '20px';
+		RiquadroApp.style.backgroundColor = '#DDDDDD';
+		RiquadroApp.style.margin = '10px 5px';
+	
+		RiquadroApp.innerHTML = ContenutoRiquadroApp;
+		document.querySelector("body > div.wrapper > div.content-wrapper").prepend(RiquadroApp);
+}
