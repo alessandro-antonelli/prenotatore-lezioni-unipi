a842b74b40ac4cd7f77e2860230b669fd1890b9b
Alessandro Antonelli <alessandroantonelli2@gmail.com>
2021-09-07 22:15:03 +0200

    scheletro

---
+++ Prenotatore lezioni UniPi.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Prenotatore lezioni UniPi
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Prenota le lezioni in presenza dell'Università di Pisa
+// @author       Alessandro Antonelli
+// @match        https://agendadidattica.unipi.it/*
+// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @grant        none
+// ==/UserScript==
+// Source code repository: https://github.com/alessandro-antonelli/prenotatore-lezioni-unipi
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
