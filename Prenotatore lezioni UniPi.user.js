7c47f897945f36c3a819e0722f4540abdde8418b
alessandro-antonelli <alessandroantonelli2@gmail.com>
2021-09-13 14:21:27 +0200

    implementata funzione AttendiCambiamentoContenutoAgenda

--- Prenotatore lezioni UniPi.user.js
+++ Prenotatore lezioni UniPi.user.js
@@ -631,6 +631,35 @@ async function RicaricaPagina(msg)
 	}
 }
 
+async function AttendiCambiamentoContenutoAgenda()
+{
+	const HashInizialeAgenda = HashStringa(document.querySelector("#calendar > div.fc-view-harness.fc-view-harness-passive").innerHTML);
+	alert(HashInizialeAgenda);
+	while(true)
+	{
+		const HashAttualeAgenda = HashStringa(document.querySelector("#calendar > div.fc-view-harness.fc-view-harness-passive").innerHTML);
+		alert(HashAttualeAgenda);
+		if(HashAttualeAgenda != HashInizialeAgenda) return;
+		else await sleep(5000);
+	}
+}
+
+function HashStringa(str, seed = 0)
+{
+	//funzione di hash: cyrb53
+	//fonte: https://stackoverflow.com/posts/52171480/
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++)
+	{
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1>>>0);
+}
+
 function LampeggioSpia()
 {
 	var spia = document.getElementById('SpiaON');
